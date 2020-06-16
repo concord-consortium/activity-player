966835428f0e2bc5e2479ee930fe617be00faff4
mklewandowski <matt.kain.lewandowski@gmail.com>
2020-06-16 09:49:13 -0700

    Adjust page title font

--- src/components/activity-introduction/activity-summary.tsx
+++ src/components/activity-introduction/activity-summary.tsx
@@ -13,7 +13,7 @@ export class ActivitySummary extends React.PureComponent <IProps> {
   render() {
     return (
       <div className="activity-summary" data-cy="activity-summary">
-        <div><h2>{this.props.activityName}</h2></div>
+        <div className="activity-title"><h2>{this.props.activityName}</h2></div>
         <div className="activity-content" dangerouslySetInnerHTML={{ __html: this.props.introText}} />
         <EstimatedTime time={this.props.time} />
       </div>
