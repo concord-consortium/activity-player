cc7691a628d4ad4e3000cebe86ee68f0be24f795
eireland <eireland@concord.org>
2020-07-13 16:31:04 -0700

    Fixes broken cypress tests. Cypress plugins folder was turned off

--- cypress/integration/activity-page.test.ts
+++ cypress/integration/activity-page.test.ts
@@ -26,19 +26,23 @@ context("Test the overall app", () => {
   });
   describe('Info/Assess (secondary embeddables)',()=>{
     it('verify image question',()=>{
-      activityPage.getSecondaryEmbeddable("image-question").eq(0).scrollIntoView().should('be.visible').and('contain',"This should be an image question");
+      activityPage.getSecondaryEmbeddable("image-question").eq(0).scrollIntoView()
+        .should('be.visible').and('contain',"background image with a Draw Tool button");
     });
     it('verify textbox',()=>{
-      activityPage.getSecondaryEmbeddable("text-box").scrollIntoView().should('be.visible').and('contain',"this is a text box");
+      activityPage.getSecondaryEmbeddable("text-box").scrollIntoView()
+        .should('be.visible').and('contain',"We want to see some text here");
     });
     it('verify multiple choice question',()=>{
       activityPage.getSecondaryEmbeddable("multiple-choice-question").scrollIntoView().should('be.visible').and('contain',"Multiple choice question");
     });
     it('verify open response question',()=>{
-      activityPage.getSecondaryEmbeddable("open-response-question").scrollIntoView().should('be.visible').and('contain',"open response question");
+      activityPage.getSecondaryEmbeddable("open-response-question").scrollIntoView()
+        .should('be.visible').and('contain',"open response question");
     });
     it('verify labbook question',()=>{
-      activityPage.getSecondaryEmbeddable("labbook-question").scrollIntoView().should('be.visible').and('contain',"Labbook type question");
+      activityPage.getSecondaryEmbeddable("labbook-question").scrollIntoView()
+        .should('be.visible').and('contain',"Labbook type question");
     });
     it('verify iframe interactive question',()=>{
       activityPage.getSecondaryEmbeddable("iframe-interactive-question").scrollIntoView().should('be.visible');
@@ -46,14 +50,17 @@ context("Test the overall app", () => {
     });
     it('verify image interactive',()=>{
       activityPage.getSecondaryEmbeddable("image-video-interactive").eq(0).scrollIntoView().should('be.visible');
-      activityPage.getSecondaryEmbeddable("image-video-interactive").eq(0).find('img').should("have.attr","src","https://media.newyorker.com/photos/5d38c026347b0e000865de67/4:3/w_1555,h_1166,c_limit/Jordan-MadMagazine-1.jpg");
+      activityPage.getSecondaryEmbeddable("image-video-interactive").eq(0).find('img')
+        .should("have.attr","src","https://media.newyorker.com/photos/5d38c026347b0e000865de67/4:3/w_1555,h_1166,c_limit/Jordan-MadMagazine-1.jpg");
     });
     it('verify video interactive',()=>{
       activityPage.getSecondaryEmbeddable("image-video-interactive").eq(1).scrollIntoView().should('be.visible');
-      activityPage.getSecondaryEmbeddable("image-video-interactive").eq(1).find('video source').should("have.attr","src","https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4");
+      activityPage.getSecondaryEmbeddable("image-video-interactive").eq(1).find('video source')
+        .should("have.attr","src","https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4");
     });
     it('verify upload image question',()=>{
-      activityPage.getSecondaryEmbeddable("image-question").eq(1).scrollIntoView().should('be.visible').and('contain',"Upload image question");
+      activityPage.getSecondaryEmbeddable("image-question").eq(1).scrollIntoView()
+        .should('be.visible').and('contain',"Upload image question");
     });
   });
 });
