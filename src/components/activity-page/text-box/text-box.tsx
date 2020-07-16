64e4e6eb8521a09c7475b1cbc2b24742fdea7c9c
mklewandowski <matt.kain.lewandowski@gmail.com>
2020-07-15 17:52:31 -0700

    Add text box header

--- src/components/activity-page/text-box/text-box.tsx
+++ src/components/activity-page/text-box/text-box.tsx
@@ -9,9 +9,10 @@ interface IProps {
 
 export class TextBox extends React.PureComponent<IProps>  {
   render () {
-    const { embeddable } =this.props;
+    const { embeddable } = this.props;
     return(
       <div className="textbox" data-cy="text-box">
+        { embeddable.name && <div className="text-name">{embeddable.name}</div> }
         <div className="content">{renderHTML(embeddable.content)}</div>
       </div>
     );
