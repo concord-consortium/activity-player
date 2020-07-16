f720025c2af9f53da8c75d93fc94f0bfd0d7eba0
Sam Fentress <sfentress@concord.org>
2020-07-16 10:15:23 -0400

    Update tests to include page loading

--- cypress/integration/load-activities.test.ts
+++ cypress/integration/load-activities.test.ts
@@ -1,10 +1,35 @@
-context("Loading sample activities", () => {
-  it("can open one sample activity",() => {
-    cy.visit("?activity=sample-activity-1");
-    cy.get("[data-cy=activity-summary]").should("contain", "Single Page Test Activity");
+import ActivityPage from "../support/elements/activity-page";
+const activityPage = new ActivityPage;
+
+context("Loading activities", () => {
+
+  describe("Loading sample activities", () => {
+    it("can open one sample activity", () => {
+      cy.visit("?activity=sample-activity-1");
+      cy.get("[data-cy=activity-summary]").should("contain", "Single Page Test Activity");
+    });
+
+    it("can open a different sample activity", () => {
+      cy.visit("?activity=sample-activity-multiple-layout-types");
+      cy.get("[data-cy=activity-summary]").should("contain", "Sample Layout Types");
+    });
   });
-  it("can open a different sample activity",() => {
-    cy.visit("?activity=sample-activity-multiple-layout-types");
-    cy.get("[data-cy=activity-summary]").should("contain", "Sample Layout Types");
+
+  describe("Loading pages", () => {
+    it("can open the introduction page by default", () => {
+      cy.visit("?activity=sample-activity-1");
+      cy.get("[data-cy=activity-summary]").should("contain", "Single Page Test Activity");
+    });
+
+    it("can load page 0", () => {
+      cy.visit("?activity=sample-activity-multiple-layout-types&page=0");
+      cy.get("[data-cy=activity-summary]").should("contain", "Sample Layout Types");
+    });
+
+    it("can load page 3", () => {
+      cy.visit("?activity=sample-activity-multiple-layout-types&page=3");
+      activityPage.getSecondaryEmbeddable("text-box").scrollIntoView()
+        .should("be.visible").and("contain","Duis vitae ultrices augue, eu fermentum elit.");
+    });
   });
 });
