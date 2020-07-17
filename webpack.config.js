b640fb9a766397e79b03931a6e45f981c7fdc411
eireland <eireland@concord.org>
2020-07-17 14:45:59 -0700

    codecov trial with cypress

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,49 @@
-'use strict';
+"use strict";
 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = (env, argv) => {
-  const devMode = argv.mode !== 'production';
+  const devMode = argv.mode !== "production";
 
   return {
     context: __dirname, // to automatically find tsconfig.json
-    devtool: 'source-map',
-    entry: './src/index.tsx',
-    mode: 'development',
+    devtool: "source-map",
+    entry: "./src/index.tsx",
+    mode: "development",
     output: {
-      filename: 'assets/index.[hash].js'
+      filename: "assets/index.[hash].js"
     },
     performance: { hints: false },
     module: {
       rules: [
         {
           test: /\.tsx?$/,
-          enforce: 'pre',
+          enforce: "pre",
           use: [
             {
-              loader: 'eslint-loader',
+              loader: "eslint-loader",
               options: {}
             }
           ]
         },
         {
           test: /\.tsx?$/,
-          loader: 'ts-loader'
+          loader: "ts-loader"
         },
         {
           test: /\.(sa|sc|c)ss$/i,
           use: [
-            devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-            'css-loader',
-            'postcss-loader',
-            'sass-loader'
+            devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader",
+            "postcss-loader",
+            "sass-loader"
           ]
         },
         {
           test: /\.(png|woff|woff2|eot|ttf)$/,
-          loader: 'url-loader',
+          loader: "url-loader",
           options: {
             limit: 8192
           }
@@ -54,18 +54,31 @@ module.exports = (env, argv) => {
             {
               // Do not apply SVGR import in CSS files.
               issuer: /\.(css|scss|less)$/,
-              use: 'url-loader'
+              use: "url-loader"
             },
             {
               issuer: /\.tsx?$/,
-              loader: '@svgr/webpack'
+              loader: "@svgr/webpack"
             }
           ]
-        }
+        },
+                // This code coverage instrumentation should only be added when needed. It makes
+        // the code larger and slower
+        process.env.CODE_COVERAGE ? {
+          test: /\.[tj]sx?$/,
+          loader: "istanbul-instrumenter-loader",
+          options: { esModules: true },
+          enforce: "post",
+          // I"m not sure but I don"t think it is necessary to exclude the cypress and
+          // tests folder. I think Jest does its own loading of tests. And cypress does
+          // its own loading of cypress
+          exclude: path.join(__dirname, "node_modules"),
+
+        } : {}
       ]
     },
     resolve: {
-      extensions: [ '.ts', '.tsx', '.js' ]
+      extensions: [ ".ts", ".tsx", ".js" ]
     },
     stats: {
       // suppress "export not found" warnings about re-exported types
@@ -76,11 +89,11 @@ module.exports = (env, argv) => {
         filename: devMode ? "assets/index.css" : "assets/index.[hash].css"
       }),
       new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: 'src/index.html'
+        filename: "index.html",
+        template: "src/index.html"
       }),
       new CopyWebpackPlugin([
-        {from: 'src/public'}
+        {from: "src/public"}
       ])
     ]
   };
