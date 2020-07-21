03d41ff868bbf732dbdcc4dff659ccc374198a8f
eireland <eireland@concord.org>
2020-07-21 08:25:22 -0700

    Changed Cypress config file to turn pluginsFolder on

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,8 @@
-import codeCoverageTask from "@cypress/code-coverage/task";
+module.exports = (on, config) => {
+  require('@cypress/code-coverage/task')(on, config)
+  // include any other plugin code...
 
-export default (on, config) => {
-  codeCoverageTask(on, config);
-  return config;
-};
+  // It's IMPORTANT to return the config object
+  // with any changed environment variables
+  return config
+}
