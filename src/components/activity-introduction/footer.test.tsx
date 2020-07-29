6d19dd49ce0d54f1f241eb0fcd48ba0d686d1bf0
eireland <eireland@concord.org>
2020-07-29 09:03:05 -0700

    adds test for default footer text when no project is specified

--- src/components/activity-introduction/footer.test.tsx
+++ src/components/activity-introduction/footer.test.tsx
@@ -9,4 +9,9 @@ describe("Footer component", () => {
     const wrapper = shallow(<Footer fullWidth={true} projectId={13} />);
     expect(wrapper.text()).toContain(footerText);
   });
+  it("renders default footer text when no project is specified", () => {
+    const footerText = "Copyright © 2020 The Concord Consortium. All rights reserved. This material is licensed under a Creative Commons Attribution 4.0 License. The software is licensed under Simplified BSD, MIT or Apache 2.0 licenses. Please provide attribution to the Concord Consortium and the URL http://concord.org.";
+    const wrapper = shallow(<Footer fullWidth={true} projectId={null} />);
+    expect(wrapper.text()).toContain(footerText);
+  });
 });
