155682cceae766abb96a02bdc7b5c5043faf0a09
mklewandowski <matt.kain.lewandowski@gmail.com>
2020-08-04 18:41:11 -0700

    Add teacher edition banner icon and test

--- src/components/teacher-edition-banner.tsx
+++ src/components/teacher-edition-banner.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import IconTeacherEdition from "../assets/svg-icons/icon-teacher-edition.svg";
 
 import "./teacher-edition-banner.scss";
 
 export const TeacherEditionBanner: React.FC = () => {
   return (
-    <div className="teacher-edition-banner">
+    <div className="teacher-edition-banner" data-cy="teacher-edition-banner">
+      <IconTeacherEdition height={32} width={32}/>
       Teacher Edition
     </div>
   );
