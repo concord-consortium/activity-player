adacc843edc7be496a994303d2d3136346c8d487
mklewandowski <matt.kain.lewandowski@gmail.com>
2020-08-06 09:58:59 -0700

    Update handling of plugin label

--- src/utilities/plugin-utils.ts
+++ src/utilities/plugin-utils.ts
@@ -1,30 +1,48 @@
 import { Activity } from "../types";
 
+type PluginType = "TeacherEdition" | "Glossary";
+export interface PluginInfo {
+  url: string;
+  type: PluginType,
+  name: string;
+  id: number;
+}
+
+// TODO: this information should come from the activity JSON
+export const Plugins: PluginInfo[] = [
+  {
+    url: "https://teacher-edition-tips-plugin.concord.org/version/v3.5.6/plugin.js",
+    type: "TeacherEdition",
+    name: "teacher edition plugin",
+    id: 0
+  },
+];
+
 export const loadPluginScripts = (activity: Activity) => {
   // load any plugin scripts, each should call registerPlugin if correctly loaded
-  // TODO: this URL should come from the activity JSON
-  const teacherEditionPluginURL = "https://teacher-edition-tips-plugin.concord.org/version/v3.5.6/plugin.js";
-  const pluginScripts: string[] = [];
+  const usedPlugins: PluginInfo[] = [];
   for (let page = 0; page < activity.pages.length - 1; page++) {
     if (!activity.pages[page].is_hidden) {
       for (let embeddableNum = 0; embeddableNum < activity.pages[page].embeddables.length; embeddableNum++) {
         const embeddable = activity.pages[page].embeddables[embeddableNum].embeddable;
         if (embeddable.type === "Embeddable::EmbeddablePlugin" && embeddable.plugin?.component_label === "windowShade") {
-          if (pluginScripts.indexOf(teacherEditionPluginURL) === -1) {
-            pluginScripts.push(teacherEditionPluginURL);
+          const plugin = Plugins.find(p => p.type === "TeacherEdition");
+          if (plugin && !usedPlugins.some(p => p.type === "TeacherEdition")) {
+            usedPlugins.push(plugin);
           }
         }
       }
     }
   }
 
-  pluginScripts.forEach((pluginScript: string) => {
+  usedPlugins.forEach((plugin) => {
     // set plugin label
-    (window as any).LARA.Plugins.setNextPluginLabel("#{v3_plugin_label}");
+    const pluginLabel = "plugin" + plugin.id;
+    (window as any).LARA.Plugins.setNextPluginLabel(pluginLabel);
     // load the script
     const script = document.createElement("script");
     script.type = "text/javascript";
-    script.src = pluginScript;
+    script.src = plugin.url;
     document.body.appendChild(script);
     script.onload = function() {
       // TODO: might need additional handling here
@@ -35,6 +53,7 @@ export const loadPluginScripts = (activity: Activity) => {
 
 export const initializePlugin = (container: HTMLElement, authoredData: string) => {
   // TODO: will need additions based as we implement other plugin types
+  const plugin = Plugins.find(p => p.type === "TeacherEdition");
   const pluginContext = {
     name: "plugin name",
     url: "plugin url",
@@ -52,5 +71,7 @@ export const initializePlugin = (container: HTMLElement, authoredData: string) =
     wrappedEmbeddable: null,
     resourceUrl: "",
   };
-  (window as any).LARA.Plugins.initPlugin("#{v3_plugin_label}", pluginContext);
+  // TODO: add sophistication to handle other types
+  const pluginLabel = "plugin" + plugin?.id;
+  (window as any).LARA.Plugins.initPlugin(pluginLabel, pluginContext);
 };
