1dc051e230b719ddc79548a73f69c88f4b2a48cb
mklewandowski <matt.kain.lewandowski@gmail.com>
2020-08-05 22:39:59 -0700

    update eslint build configuration

--- .eslintrc.build.js
+++ .eslintrc.build.js
@@ -4,15 +4,5 @@ module.exports = {
     rules: {
       "no-console": ["warn", { allow: ["warn", "error"] }],
       "no-debugger": "error"
-    },
-    overrides: [
-      {
-        files: ["./src/lara-plugin/**/*.ts"],
-        rules: {
-          "@typescript-eslint/ban-ts-comment": "off",
-          "@typescript-eslint/no-non-null-assertion": "off",
-          "no-console": ["warn", { allow: ["warn", "error", "group", "groupEnd", "dir", "info"] }],
-        }
-      }
-    ]
+    }
 };
