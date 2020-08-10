9e947f2105288ef7cec5dcdfe0a06246d0f0e7c8
Sam Fentress <sfentress@concord.org>
2020-08-10 09:49:43 -0400

    Update embeddable tests to support async change

--- src/components/activity-page/embeddable.test.tsx
+++ src/components/activity-page/embeddable.test.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { waitFor } from "@testing-library/dom";
 import { Embeddable } from "./embeddable";
-import { TextBox } from "./text-box/text-box";
 import { PageLayouts } from "../../utilities/activity-utils";
-import { shallow } from "enzyme";
+import { mount, ReactWrapper } from "enzyme";
 import { EmbeddableWrapper } from "../../types";
+import { act } from "react-dom/test-utils";
 
 describe("Embeddable component", () => {
-  it("renders component", () => {
+  it("renders component", async () => {
     const embeddableWrapper: EmbeddableWrapper = {
       "embeddable": {
         "content": "<p><strong>This is a page with full width layout</strong>.&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>",
@@ -18,7 +19,15 @@ describe("Embeddable component", () => {
       },
       "section": "header_block"
     };
-    const wrapper = shallow(<Embeddable embeddableWrapper={embeddableWrapper} isPageIntroduction={false} questionNumber={1} pageLayout={PageLayouts.Responsive}/>);
-    expect(wrapper.find(TextBox).length).toBe(1);
+
+    let wrapper: ReactWrapper;
+    await act(async () => {
+      wrapper = mount(<Embeddable embeddableWrapper={embeddableWrapper} isPageIntroduction={false} questionNumber={1} pageLayout={PageLayouts.Responsive}/>);
+      expect(wrapper.containsMatchingElement(<div>Loading...</div>)).toEqual(true);
+    });
+
+    await waitFor(() => {
+      expect(wrapper.text()).toContain("This is a page");
+    });
   });
 });
