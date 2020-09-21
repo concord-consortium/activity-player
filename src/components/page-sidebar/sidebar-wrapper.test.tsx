8f381b74d85ed46574c7cb72b6c567a085ddcfbb
mklewandowski <matt.kain.lewandowski@gmail.com>
2020-09-21 15:31:16 -0700

    Update position of sidebar

--- src/components/page-sidebar/sidebar-wrapper.test.tsx
+++ src/components/page-sidebar/sidebar-wrapper.test.tsx
@@ -6,7 +6,7 @@ import { shallow } from "enzyme";
 describe("Sidebar Container component", () => {
   it("renders container component", () => {
     const sidebars: SidebarConfiguration[] = [{content: "sidebar", title: "sidebar1"}, {content: "sidebar", title: "sidebar2"}];
-    const wrapper = shallow(<SidebarWrapper sidebars={sidebars}/>);
+    const wrapper = shallow(<SidebarWrapper sidebars={sidebars} verticalOffset={200} />);
     expect(wrapper.find(Sidebar).length).toBe(2);
   });
 });
