2d3b7fdf6ebee7c595e69b97e0c6ce936069369d
Kirk Swenson <kswenson@concord.org>
2020-09-25 06:43:15 -0700

    More LARA Synchronization

--- src/lara-plugin/index.ts
+++ src/lara-plugin/index.ts
@@ -12,35 +12,24 @@ export interface LaraGlobalType {
   Events: typeof Events;
 }
 
-// export {
-//   PluginAPI as PluginAPI_V3,
-//   // LARA_CODE InteractiveAPI,
-//   Plugins,
-//   // LARA_CODE Events,
-//   // LARA_CODE PageItemAuthoring
-// };
+export {
+  PluginAPI as PluginAPI_V3,
+  // LARA_CODE InteractiveAPI,
+  Plugins,
+  Events,
+  // LARA_CODE PageItemAuthoring
+};
 
+// Note that LARA namespace is defined for the first time by V2 API. Once V2 is removed, this code should also be
+// removed and "library": "LARA" option in webpack.config.js should be re-enabled.
+(window as any).LARA || ((window as any).LARA = {});  // create if it doesn't exist
+(window as any).LARA.PluginAPI_V3 = PluginAPI;
+(window as any).LARA.Plugins = Plugins;
+(window as any).LARA.Events = Events;
+// LARA_CODE (window as any).LARA.InteractiveAPI = InteractiveAPI;
+// LARA_CODE (window as any).LARA.PageItemAuthoring = PageItemAuthoring;
+
+// for clients that don't require LARA to be a global on window
 export function initializeLara(): LaraGlobalType {
-  const LARA: LaraGlobalType = {
-    PluginAPI_V3: PluginAPI,
-    Plugins,
-    Events
-  };
-  // plugins require this global
-  (window as any).LARA = LARA;
-  return LARA;
+  return (window as any).LARA;
 }
-
-// ACTIVITY_PLAYER_CODE: wrap namespace creation into function createPluginNamespace
-// export const createPluginNamespace = () => {
-//   // add empty LARA namespace to window
-//   (window as any).LARA = {};
-
-//   // Note that LARA namespace is defined for the first time by V2 API. Once V2 is removed, this code should also be
-//   // removed and "library": "LARA" option in webpack.config.js should be re-enabled.
-//   (window as any).LARA.PluginAPI_V3 = PluginAPI;
-//   (window as any).LARA.Plugins = Plugins;
-//   // LARA_CODE (window as any).LARA.Events = Events;
-//   // LARA_CODE (window as any).LARA.InteractiveAPI = InteractiveAPI;
-//   // LARA_CODE (window as any).LARA.PageItemAuthoring = PageItemAuthoring;
-// };
