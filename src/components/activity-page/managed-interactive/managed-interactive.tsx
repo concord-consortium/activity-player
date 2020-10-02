64879b883838601fd20c8d27ed1c11e4de065c81
pjanik <janikpiotrek@gmail.com>
2020-10-02 19:02:32 +0200

    Implement snapshot API
    [#173594575]

--- src/components/activity-page/managed-interactive/managed-interactive.tsx
+++ src/components/activity-page/managed-interactive/managed-interactive.tsx
@@ -66,6 +66,8 @@ export const ManagedInteractive: React.FC<IProps> = (props) => {
     const linkedInteractives = useRef((embeddable.type === "ManagedInteractive") && embeddable.linked_interactives?.length
                                   ? embeddable.linked_interactives.map(link => ({ id: link.ref_id, label: link.label }))
                                   : undefined);
+    // This value should not be changed, and it should always match IDs generated above in linkedInteractives array.
+    const interactiveId = embeddable.ref_id;
     // TODO: handle different aspect ratio methods
     // const aspectRatioMethod = data.aspect_ratio_method ? data.aspect_ratio_method : "";
     const nativeHeight = embeddableData?.native_height || 0;
@@ -120,6 +122,7 @@ export const ManagedInteractive: React.FC<IProps> = (props) => {
     const interactiveIframe =
       <IframeRuntime
         url={url}
+        id={interactiveId}
         authoredState={authoredState}
         initialInteractiveState={iframeInteractiveState.current}
         setInteractiveState={handleNewInteractiveState}
