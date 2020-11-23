4d56306e1793b86b17d2d875f78148688b02b17e
pjanik <janikpiotrek@gmail.com>
2020-11-23 20:45:35 +0100

    Render interactive iframes with allow="geolocation *; microphone *; camera *"
    [#174502804]
    
    To let Vortex access camera

--- src/components/activity-page/managed-interactive/iframe-runtime.tsx
+++ src/components/activity-page/managed-interactive/iframe-runtime.tsx
@@ -236,7 +236,10 @@ export const IframeRuntime: React.ForwardRefExoticComponent<IProps> = forwardRef
 
   return (
     <div data-cy="iframe-runtime">
-      <iframe ref={iframeRef} src={url} id={id} width="100%" height={height} frameBorder={0} />
+      <iframe ref={iframeRef} src={url} id={id} width="100%" height={height} frameBorder={0}
+              allowFullScreen={true}
+              allow="geolocation *; microphone *; camera *"
+      />
     </div>
   );
 });
