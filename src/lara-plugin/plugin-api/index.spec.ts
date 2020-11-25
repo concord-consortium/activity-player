3dd59f834e185526228599e1bc5c17a03aca5414
mklewandowski <matt.kain.lewandowski@gmail.com>
2020-11-24 20:35:58 -0800

    restore tests

--- src/lara-plugin/plugin-api/index.spec.ts
+++ src/lara-plugin/plugin-api/index.spec.ts
@@ -9,9 +9,9 @@ import * as PluginAPI from "./index";
 describe("Plugin API", () => {
   it("should export all the API functions", () => {
     expect(PluginAPI.registerPlugin).toBeDefined();
-    // LARA_CODE expect(PluginAPI.addSidebar).toBeDefined();
-    // LARA_CODE expect(PluginAPI.addPopup).toBeDefined();
-    // LARA_CODE expect(PluginAPI.decorateContent).toBeDefined();
+    expect(PluginAPI.addSidebar).toBeDefined();
+    expect(PluginAPI.addPopup).toBeDefined();
+    expect(PluginAPI.decorateContent).toBeDefined();
     // LARA_CODE expect(PluginAPI.events).toBeDefined();
   });
 });
