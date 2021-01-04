a12c7a5a371af2e27ff5d92b3471d47c7aff576e
mklewandowski <matt.kain.lewandowski@gmail.com>
2021-01-04 12:01:28 -0800

    Update logo component in header

--- src/components/activity-header/logo.tsx
+++ src/components/activity-header/logo.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { accessibilityClick } from "../../utilities/accessibility-helper";
+import CCLogo from "../../assets/svg-icons/cclogo.svg";
 
 import "./logo.scss";
 
@@ -16,11 +17,15 @@ export class Logo extends React.PureComponent<IProps> {
 
     return (
       <div className={`project-logo ${linkClass}`}
-            data-cy="project-logo"
-            onClick={this.handleProjectLogoLink(url)}
-            onKeyDown={this.handleProjectLogoLink(url)}
-            tabIndex={tIndex} >
-        <img data-cy="logo-img" className="logo-img" src={logo} alt="Project website"/>
+           data-cy="project-logo"
+           onClick={this.handleProjectLogoLink(url)}
+           onKeyDown={this.handleProjectLogoLink(url)}
+           tabIndex={tIndex}
+      >
+        { logo
+          ? <img data-cy="logo-img" className="logo-img" src={logo} alt="Project website"/>
+          : <CCLogo className="icon" />
+        }
       </div>
     );
   }
