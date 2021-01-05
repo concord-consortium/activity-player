760a270f3959e60a26a3f899c4c26dd0c6169e72
mklewandowski <matt.kain.lewandowski@gmail.com>
2021-01-04 16:14:37 -0800

    Update page intro

--- src/components/activity-introduction/introduction-page-content.tsx
+++ src/components/activity-introduction/introduction-page-content.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { SocialMediaLinks } from "../activity-introduction/social-media-links";
 import { ActivitySummary } from "../activity-introduction/activity-summary";
 import { ActivityPageLinks } from "../activity-introduction/activity-page-links";
 
@@ -15,7 +14,6 @@ export const IntroductionPageContent: React.FC<IProps> = (props) => {
   const { activity, onPageChange } = props;
   return (
     <div className="intro-content" data-cy="intro-page-content">
-      <SocialMediaLinks shareURL="https://concord.org/" />
       <div className="introduction">
         <ActivitySummary
           activityName={activity.name}
