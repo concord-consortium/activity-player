9d39d72ba64c82687dc366ea06de8145be27d89d
mklewandowski <matt.kain.lewandowski@gmail.com>
2021-01-04 16:14:28 -0800

    Update page intro nav links component

--- src/components/activity-introduction/activity-page-links.tsx
+++ src/components/activity-introduction/activity-page-links.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-
-import "./activity-page-links.scss";
 import { Page } from "../../types";
 import { accessibilityClick } from "../../utilities/accessibility-helper";
+import "./activity-page-links.scss";
 
 interface IProps {
   activityPages: Page[];
@@ -14,23 +13,26 @@ export class ActivityPageLinks extends React.PureComponent <IProps> {
     return (
       <div className="activity-page-links" data-cy="activity-page-links">
         <div className="pages">Pages in this Activity</div>
+        <div className="separator" />
         { this.props.activityPages.filter((page) => !page.is_hidden).map((page, index: number) => (
-          <div className="page-item" key={`index ${index}`}
-               onClick={this.handlePageChange(index + 1)}
-               onKeyDown={this.handlePageChange(index + 1)}>
-            <span>{`${index + 1}. `}</span>
-            <span className="page-link"
-                  onClick={this.handlePageChange(index + 1)}
-                  onKeyDown={this.handlePageChange(index + 1)}
-                  tabIndex={0}>{`Page ${index + 1}`}
-            </span>
+          <div
+            className="page-item"
+            key={`index ${index}`}
+            onClick={this.handlePageChange(index + 1)}
+            onKeyDown={this.handlePageChange(index + 1)}
+            tabIndex={0}
+          >
+            <span>{`${index + 1}: `}</span>
+            <span>{`Page ${index + 1}`}</span>
           </div>
           ))
         }
-        <button className="button begin"
-                onClick={this.handlePageChange(1)}
-                onKeyDown={this.handlePageChange(1)}>
-                Begin Activity
+        <button
+          className="button begin"
+          onClick={this.handlePageChange(1)}
+          onKeyDown={this.handlePageChange(1)}
+        >
+          Begin Activity
         </button>
       </div>
     );
