7afba7b64404cdb20139db04d6141300817b6556
mklewandowski <matt.kain.lewandowski@gmail.com>
2021-01-04 17:20:01 -0800

    Fix unit test

--- src/components/activity-introduction/estimated-time.test.tsx
+++ src/components/activity-introduction/estimated-time.test.tsx
@@ -4,7 +4,7 @@ import { shallow } from "enzyme";
 
 describe("Estimated Time component", () => {
   it("renders estimated time text", () => {
-    const estimatedTimeText = "Estimated Time to Complete This Module:10minutes";
+    const estimatedTimeText = "Estimated Time to Complete This Module:10 minutes";
     const wrapper = shallow(<EstimatedTime time={10} />);
     expect(wrapper.text()).toContain(estimatedTimeText);
   });
