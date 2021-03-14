bef946edcdee6c501e84889a52bcc76870a5fa8f
Scott Cytacki <scytacki@concord.org>
2021-03-13 23:32:41 -0500

    use a single version
    this also changes the name of the database to avoid migration issues

--- src/storage/dexie-storage.ts
+++ src/storage/dexie-storage.ts
@@ -3,7 +3,8 @@ import { OfflineActivity, LogMessage } from "../types";
 import { IIndexedDBAnswer } from "./storage-facade";
 
 // We need to ensure a version match between data stored and exported
-export const kOfflineAnswerSchemaVersion = 4;
+// version 5: switched from activity to resource_url field for identifing answers's activity
+export const kOfflineAnswerSchemaVersion = 5;
 
 // Copy and pasted from the example: https://dexie.org/docs/Typescript
 export class DexieStorage extends Dexie {
@@ -15,28 +16,16 @@ export class DexieStorage extends Dexie {
   pluginStates: Dexie.Table<{pluginId: number, state: string|null}>;
 
   constructor () {
-      super("ActivityPlayer");
-      // FIXME: There is only one version for the whole database.
-      // calling version multiple times is how you can specify a schema for older
-      // versions of the database.
-      this.version(5).stores({
-        logs: "++id, activity"
-      });
-      this.version(kOfflineAnswerSchemaVersion).stores({
-          answers: "id, resource_url, [resource_url+question_id]",
-          pluginStates: "&pluginId"
-      });
-      this.version(3).stores({
-        offlineActivities: "&resourceUrl"  // unique by resourceUrl
-      });
-
-      // Once we fix the version issue, the following code can be used
-      // to clear the old tables since we don't need to worry about
-      // about the old data. I'm not sure what the return value should
-      // be to the upgrade callback
-      // .upgrade(tx => {
-      //   return tx.table("offlineActivities").toCollection().delete();
-      // });
+    // the database was called ActivityPlayer, but changes to the offlineActivities
+    // primary key required deleting that table to upgrade it, so renaming
+    // the database was easier
+    super("ActivityPlayerDb");
+    this.version(kOfflineAnswerSchemaVersion).stores({
+      logs: "++id, activity",
+      answers: "id, resource_url, [resource_url+question_id]",
+      pluginStates: "&pluginId",
+      offlineActivities: "&resourceUrl"  // unique by resourceUrl
+    });
   }
 }
 
