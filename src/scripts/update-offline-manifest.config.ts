5e2704a9f8d3e1a7cae45893b192431116b0926d
Tejal Shah <tejal.shah@tscube.com>
2021-07-08 00:30:13 +0530

    Updating APO manifest to use Glossary Plugin 3.13.0

--- src/scripts/update-offline-manifest.config.ts
+++ src/scripts/update-offline-manifest.config.ts
@@ -3,8 +3,8 @@
 export const config = {
   glossary: {
     s3Url: "https://models-resources.s3.amazonaws.com/glossary-resources/custom-apo/alaska-v2.json?__noUrlRewrite",
-    pluginUrl: "https://glossary-plugin.concord.org/version/v3.13.0-pre.3/plugin.js",
-    manifestUrl: "https://glossary-plugin.concord.org/version/v3.13.0-pre.3/manifest.json"
+    pluginUrl: "https://glossary-plugin.concord.org/version/v3.13.0/plugin.js",
+    manifestUrl: "https://glossary-plugin.concord.org/version/v3.13.0/manifest.json"
   },
   questionInteractives: {
     version: "v1.2.0-pre.1"
