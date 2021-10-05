4211f8247e52aee43fc5a4739b2c2a5b5e14d983
Doug Martin <dmartin@concord.org>
2021-10-05 11:46:36 -0400

    Added jest app component tests to check fixed width classes

--- src/components/app.test.tsx
+++ src/components/app.test.tsx
@@ -13,10 +13,12 @@ import { GlossaryPlugin } from "../components/activity-page/plugins/glossary-plu
 import _activitySinglePage from "../data/sample-activity-single-page-layout.json";
 import _activity from "../data/sample-activity-multiple-layout-types.json";
 import _activityWithGlossary from "../data/sample-activity-glossary-plugin.json";
+import _activityOnIpad from "../data/sample-activity-ipad-friendly.json";
 
 const activity = _activity as Activity;
 const activityWithGlossary = _activityWithGlossary as Activity;
 const activitySinglePage = _activitySinglePage as Activity;
+const activityOnIpad = _activityOnIpad as Activity;
 
 describe("App component", () => {
   it("renders component", () => {
@@ -27,12 +29,21 @@ describe("App component", () => {
     expect(wrapper.find(Header).length).toBe(1);
     expect(wrapper.find(Footer).length).toBe(1);
   });
-  it("renders single page activity", () => {
+  it("renders single page activity at the default fixed width", () => {
     const wrapper = shallow(<App />);
     wrapper.setState({ activity: activitySinglePage });
     expect(wrapper.find(ActivityNav).length).toBe(0);
     expect(wrapper.find(Header).length).toBe(1);
     expect(wrapper.find(Footer).length).toBe(1);
+    expect(wrapper.find(".activity").length).toBe(1);
+    expect(wrapper.find(".fixed-width-1100px").length).toBe(1);
+    expect(wrapper.find(".fixed-width-ipad-friendly").length).toBe(0);
+  });
+  it("renders an ipad activity at the ipad friendly fixed width", () => {
+    const wrapper = shallow(<App />);
+    wrapper.setState({ activity: activityOnIpad });
+    expect(wrapper.find(".fixed-width-1100px").length).toBe(0);
+    expect(wrapper.find(".fixed-width-ipad-friendly").length).toBe(1);
   });
   it("loads plugin scripts", () => {
     const wrapper = shallow(<App />);
