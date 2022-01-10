85c438b916fd1c7a9ee9e3abe334122e558f0bbb
Evangeline Ireland <eireland@concord.org>
2022-01-10 18:54:39 -0500

    Fixes tab title when there are hidden pages in the activity (#304)

--- src/utilities/activity-utils.ts
+++ src/utilities/activity-utils.ts
@@ -163,9 +163,10 @@ export const setAppBackgroundImage = (backgroundImageUrl?: string) => {
 
 export const setDocumentTitle = (activity: Activity | undefined, pageNumber: number) => {
   if (activity) {
+    const visiblePages = activity.pages.filter(p => !p.is_hidden);
     document.title = pageNumber === 0
       ? activity.name
-      : `Page ${pageNumber} ${activity.pages[pageNumber - 1].name || activity.name}`;
+      : `Page ${pageNumber} ${visiblePages[pageNumber - 1].name || activity.name}`;
   }
 };
 
