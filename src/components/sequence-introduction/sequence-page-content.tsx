97234269ce722d952049b6035565550af6724635
lublagg <lvblagg@gmail.com>
2025-01-21 18:04:53 -0500

    checkpoint: show banner + badges if any activity-level or question-level feedback in sequence

--- src/components/sequence-introduction/sequence-page-content.tsx
+++ src/components/sequence-introduction/sequence-page-content.tsx
@@ -1,11 +1,12 @@
 import { DynamicText } from "@concord-consortium/dynamic-text";
 import React, { useEffect } from "react";
-import { Activity, AllTeacherFeedback, Sequence } from "../../types";
+import { Activity, Sequence } from "../../types";
 import { renderHTML } from "../../utilities/render-html";
 import { EstimatedTime } from "../activity-introduction/estimated-time";
 import { ReadAloudToggle } from "../read-aloud-toggle";
 import { ActivityLayoutOverrides } from "../../utilities/activity-utils";
-import { getAllTeacherFeedback } from "../../firebase-db";
+import { watchActivityFeedbackForSequence, watchAllQuestionLevelFeedback } from "../../firebase-db";
+import { refIdToAnswersQuestionId } from "../../utilities/embeddable-utils";
 import { SequenceIntroFeedbackBanner } from "./sequence-intro-feedback-banner";
 import { FeedbackBadge } from "./feedback-badge";
 
@@ -21,26 +22,32 @@ export const SequencePageContent: React.FC<IProps> = (props) => {
   const isNotebookLayout = sequence.layout_override === ActivityLayoutOverrides.Notebook;
   let totalTime = 0;
   let stubCount = 0;
-  const [feedbacks, setFeedbacks] = React.useState<Record<number, AllTeacherFeedback>>({});
-  const [hasFeedback, setHasFeedback] = React.useState<boolean>(false);
+  const [activitiesWithActivityLevelFeedback, setActivitiesWithActivityLevelFeedback] = React.useState<string[]>([]);
+  const [activitiesWithQuestionLevelFeedback, setActivitiesWithQuestionLevelFeedback] = React.useState<string[]>([]);
 
   sequence.activities.forEach((a: Activity) => totalTime += a.time_to_complete || 0);
 
   useEffect(() => {
-    async function fetchFeedback() {
-      const activityFeedbacks: Record<number, AllTeacherFeedback> = {};
-      sequence.activities.forEach(async (a: Activity) => {
-        const activityFeedback = a.id ? await getAllTeacherFeedback(a.id, true) : null;
-        if (!a.id || !activityFeedback) return;
-        activityFeedbacks[a.id] = activityFeedback;
-        if (activityFeedback.activityFeedback?.[0] || activityFeedback.questionFeedback?.length) {
-          setHasFeedback(true);
-        }
-      });
-      setFeedbacks(activityFeedbacks);
+    async function watchAllActivityFeedback() {
+      return watchActivityFeedbackForSequence((ids: string[]) => setActivitiesWithActivityLevelFeedback(ids));
     }
-    fetchFeedback();
-  }, [sequence.activities, setFeedbacks]);
+
+    async function watchAllQuestionFeedback() {
+      return watchAllQuestionLevelFeedback((questionIds: string[]) => {
+        const filteredByQuestionFeedback = sequence.activities.filter(a =>
+          a.pages.some(p =>
+            p.sections.some(s =>
+              s.embeddables.some(e => questionIds.includes(refIdToAnswersQuestionId(e.ref_id)))
+            )
+          )
+        ).map(activity => activity.id).filter(Boolean).map(String);
+        setActivitiesWithQuestionLevelFeedback(filteredByQuestionFeedback);
+      });
+    }
+
+    watchAllActivityFeedback();
+    watchAllQuestionFeedback();
+  }, [sequence.activities]);
 
   return (
     <div className="sequence-content" data-cy="sequence-page-content">
@@ -51,17 +58,17 @@ export const SequencePageContent: React.FC<IProps> = (props) => {
             <div className="sequence-title">
               <h2><DynamicText>{sequence.display_title || sequence.title || ""}</DynamicText></h2>
             </div>
-            <ReadAloudToggle/>
+            <ReadAloudToggle />
           </div>
-          <div className="description">{ sequence.description && renderHTML(sequence.description) }</div>
+          <div className="description">{sequence.description && renderHTML(sequence.description)}</div>
           <EstimatedTime time={totalTime} />
-          {hasFeedback && <SequenceIntroFeedbackBanner />}
+          {activitiesWithActivityLevelFeedback.length > 0 && <SequenceIntroFeedbackBanner />}
           <div className="thumb-holder">
             {sequence.activities.map((a: Activity, index: number) => {
               if (!a.thumbnail_url) {
                 stubCount++;
               }
-              const activityHasFeedback = a.id && feedbacks[a.id]?.activityFeedback?.[0];
+              const activityHasFeedback = a.id && (activitiesWithActivityLevelFeedback.includes(`${a.id}`) || activitiesWithQuestionLevelFeedback.includes(`${a.id}`));
               return (
               <div className="thumb" key={`activity-${index}`} onClick={() => onSelectActivity(index)} data-cy="sequence-thumb">
                 {activityHasFeedback && <FeedbackBadge />}
