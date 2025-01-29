1583cd3c5a65e7faf685b541cee24499ddc54fac
lublagg <lvblagg@gmail.com>
2025-01-29 13:59:10 -0500

    feat: show feedback badges on section tabs

--- src/components/activity-page/activity-page-content.tsx
+++ src/components/activity-page/activity-page-content.tsx
@@ -5,17 +5,30 @@ import classNames from "classnames";
 import { Section, SectionImperativeAPI } from "./section";
 import { BottomButtons } from "./bottom-buttons";
 import { ActivityLayouts, numQuestionsOnPreviousSections } from "../../utilities/activity-utils";
-import { Activity, Page, SectionType } from "../../types";
+import { Activity, Page, QuestionFeedback, SectionType } from "../../types";
 import { IGetInteractiveState, INavigationOptions } from "@concord-consortium/lara-interactive-api";
+import { QuestionInfoContext } from "../question-info-context";
+import { answersQuestionIdToRefId } from "../../utilities/embeddable-utils";
+import { watchQuestionLevelFeedback } from "../../firebase-db";
 import { Logger, LogEventName } from "../../lib/logger";
 import { showReport } from "../../utilities/report-utils";
 import { IPageChangeNotification, PageChangeNotification } from "./page-change-notification";
 import { ReadAloudToggle } from "../read-aloud-toggle";
+import { TeacherFeedbackSmallBadge } from "../teacher-feedback/teacher-feedback-small-badge";
 
 import "./activity-page-content.scss";
 
-const SectionTab = (props: { label: string, zIndex: number, section: SectionType, selected: boolean, onSelected: (section: SectionType) => void }) => {
-  const { label, zIndex, section, selected, onSelected } = props;
+interface ISectionTabProps {
+  label: string;
+  zIndex: number;
+  section: SectionType;
+  selected: boolean;
+  onSelected: (section: SectionType) => void;
+  hasFeedback: boolean;
+}
+
+const SectionTab = (props: ISectionTabProps) => {
+  const { label, zIndex, section, selected, onSelected, hasFeedback } = props;
   const className = classNames("section-tab", { selected });
   const handleClick = () => onSelected(section);
   const style: React.CSSProperties = {zIndex};
@@ -23,6 +36,7 @@ const SectionTab = (props: { label: string, zIndex: number, section: SectionType
   return (
     <div className={className} style={style} onClick={handleClick}>
       {label}
+      {hasFeedback && <TeacherFeedbackSmallBadge/>}
     </div>
   );
 };
@@ -45,17 +59,43 @@ interface IProps {
 
 interface IState {
   selectedSection: SectionType | undefined;
+  questionsWithFeedback: string[];
 }
 
 export class ActivityPageContent extends React.Component<IProps, IState> {
+  static contextType = QuestionInfoContext;
+
   private sectionRefs: Record<string, React.RefObject<SectionImperativeAPI>> = {};
+  private unsubscribeQuestionLevelFeedback?: () => void;
 
   public constructor(props: IProps) {
     super(props);
 
     this.state = {
-      selectedSection: props.page.sections[0]
+      selectedSection: props.page.sections[0],
+      questionsWithFeedback: []
     };
+  }
+
+  componentDidMount() {
+    const { activity, page } = this.props;
+    const { questionMap } = this.context;
+    if (activity.id) {
+      this.unsubscribeQuestionLevelFeedback = watchQuestionLevelFeedback((fbs: QuestionFeedback[]) => {
+        const qIds: string[] = [];
+        fbs.forEach(fb => {
+          const eRefId = answersQuestionIdToRefId(fb.questionId);
+          if (questionMap[eRefId]?.pageId === page.id) {
+            qIds.push(eRefId);
+          }
+        });
+        this.setState({ questionsWithFeedback: qIds });
+      });
+    }
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeQuestionLevelFeedback?.();
   }
 
   renderPageChangeNotification() {
@@ -156,12 +196,14 @@ export class ActivityPageContent extends React.Component<IProps, IState> {
   }
 
   private renderTabs(sections: SectionType[]) {
+    const { questionsWithFeedback } = this.state;
     return (
       <>
       <div className="section-tabs">
         {sections.map((section, i) => (
           <SectionTab
             key={i}
+            hasFeedback={section.embeddables.some(e => questionsWithFeedback.includes(e.ref_id))}
             zIndex={sections.length - i}
             label={section.name || `Tab ${i + 1}`}
             section={section}
