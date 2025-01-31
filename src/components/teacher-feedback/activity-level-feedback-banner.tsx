60ab3ec99400c41e474a6608a61482102a2ceeed
Ethan McElroy <emcelroy@concord.org>
2025-01-30 20:10:14 -0500

    fix: feedback header styling, failing test

--- src/components/teacher-feedback/activity-level-feedback-banner.tsx
+++ src/components/teacher-feedback/activity-level-feedback-banner.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import classNames from "classnames";
 import { ActivityFeedback } from "../../types";
 import { RubricComponent } from "./rubric";
 import TeacherFeedbackIcon from "../../assets/svg-icons/teacher-feedback-icon.svg";
@@ -11,15 +12,18 @@ interface IProps {
 
 export const ActivityLevelFeedbackBanner = ({ teacherFeedback }: IProps) => {
   const hasRubric = !!teacherFeedback.feedbackSettings?.rubric;
+  const bannerClass = classNames("activity-level-feedback-banner", {
+    "has-rubric": hasRubric
+  });
 
   return (
-    <div className="activity-level-feedback-banner" data-testid="activity-level-feedback-banner">
+    <div className={bannerClass} data-testid="activity-level-feedback-banner">
       <TeacherFeedbackIcon className="teacher-feedback-icon" />
-      <div className="activity-level-feedback-title" data-testid="activity-level-feedback-title">
-        <strong>Overall Teacher Feedback for This Activity:</strong>
-      </div>
+      {hasRubric && <div className="activity-level-feedback-title"><strong>Overall Teacher Feedback for This Activity:</strong> </div>}
       <div className="activity-level-feedback-content" data-testid="activity-level-feedback-content">
-        {hasRubric ? <RubricComponent teacherFeedback={teacherFeedback} /> : teacherFeedback.content}
+        {hasRubric
+          ? <RubricComponent teacherFeedback={teacherFeedback} />
+          : <><strong>Overall Teacher Feedback for This Activity:</strong> {teacherFeedback.content}</>}
       </div>
     </div>
   );
